47fcfc1faf43168430233c97d398d0e657e668e4
Guilherme Agostinelli <agostinelli.guilherme@gmail.com>
2021-01-02 18:04:16 -0300

    Add command center functionality

---
+++ lib/Notifiers/CommandCenterNotifier.dart
@@ -0,0 +1,120 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:ball_thrower_app/Config/Config.dart';
+import 'package:ball_thrower_app/Enums/Speed.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:asuka/asuka.dart' as asuka;
+
+class CommandCenterNotifier extends ChangeNotifier {
+  Dio _dio = Dio(
+    BaseOptions(
+      baseUrl: Config.serverUrl,
+      connectTimeout: 300,
+      receiveTimeout: 300,
+      sendTimeout: 300,
+    ),
+  );
+  bool _serverOn = false;
+  bool _ballThrowerOn = false;
+  Speed _feederSpeed = Speed.MEDIUM;
+  Speed _shooterSpeed = Speed.MEDIUM;
+
+  Future<void> _reconnectToServer() async {
+    _serverOn = false;
+    notifyListeners();
+    await connectToServer();
+  }
+
+  Future<void> connectToServer() async {
+    try {
+      final result = await InternetAddress.lookup('${Config.serverUrl}/status');
+      _serverOn = (result.isNotEmpty && result[0].rawAddress.isNotEmpty);
+    } on SocketException catch (_) {
+      _serverOn = false;
+    }
+    if (!_serverOn) {
+      await Future.delayed(const Duration(seconds: 2));
+      return await connectToServer();
+    }
+    notifyListeners();
+  }
+
+  Future<bool> _sendCommandToServer(String path, String errActionMsg) async {
+    try {
+      final res = await _dio.get(Config.serverUrl + '/' + path);
+      return res.statusCode == 200;
+    } catch (err) {
+      asuka.showDialog(
+        builder: (context) => AlertDialog(
+          title: Text('Error'),
+          content: Text(
+            'An error occurred while trying to $errActionMsg:\n\n$err',
+          ),
+          actions: [
+            FlatButton(
+              child: Text('Close'),
+              onPressed: () => Navigator.of(context).pop(),
+            )
+          ],
+        ),
+      );
+      return false;
+    }
+  }
+
+  set ballThrowerOn(bool isOn) {
+    if (isOn) {
+      Future.wait([
+        _sendCommandToServer('feeder/start', 'start the feeder'),
+        _sendCommandToServer('shooter/start', 'start the shooter'),
+      ]).then((responses) {
+        if (!responses.every((r) => r)) return _reconnectToServer();
+        _ballThrowerOn = true;
+        notifyListeners();
+      });
+    } else {
+      Future.wait([
+        _sendCommandToServer('feeder/stop', 'stop the feeder'),
+        _sendCommandToServer('shooter/stop', 'stop the shooter'),
+      ]).then((responses) {
+        if (!responses.every((r) => r)) return _reconnectToServer();
+        _ballThrowerOn = false;
+        _feederSpeed = Speed.MEDIUM;
+        _shooterSpeed = Speed.MEDIUM;
+        notifyListeners();
+      });
+    }
+  }
+
+  set feederSpeed(Speed speed) {
+    _sendCommandToServer(
+      'feeder/change-speed/${speed.valueOf}',
+      'change the feeder speed',
+    ).then((r) {
+      if (!r) return _reconnectToServer();
+      _feederSpeed = speed;
+      notifyListeners();
+    });
+  }
+
+  set shooterSpeed(Speed speed) {
+    _sendCommandToServer(
+      'shooter/change-speed/${speed.valueOf}',
+      'change the shooter speed',
+    ).then((r) {
+      if (!r) return _reconnectToServer();
+      _shooterSpeed = speed;
+      notifyListeners();
+    });
+  }
+
+  get ballThrowerOn => _ballThrowerOn;
+
+  get feederSpeed => _feederSpeed;
+
+  get shooterSpeed => _shooterSpeed;
+
+  get serverOn => _serverOn;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:ball_thrower_app/Notifiers/CommandCenterNotifier.dart';
+import 'package:ball_thrower_app/Pages/CommandCenterPage.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:asuka/asuka.dart' as asuka;
 
 void main() {
   runApp(MyApp());
@@ -7,60 +11,22 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (_) => CommandCenterNotifier(),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      ],
+      child: MaterialApp(
+        title: 'Ball Thrower',
+        theme: ThemeData(
+          primarySwatch: Colors.orange,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: CommandCenterPage(),
+        builder: asuka.builder,
       ),
     );
   }
 }
+
